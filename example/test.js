f074c3028084ce7375db4dc4d4001d7dd73697cb
lrmn7 <lrmn.dev@gmail.com>
2024-01-18 03:55:41 +0700

    ✏️ fix typos

--- example/test.js
+++ example/test.js
@@ -1,20 +1,18 @@
 (async () => {
     const { mewcard } = require("mewcard");
     const fs = require("fs");
-
-    const card = new mewcard()
-        .setName("Biru Baru")
-        .setAuthor("Sejenak")
-        .setColor("auto")
-        .setTheme("classic")
-        .setBrightness(50)
-        .setThumbnail("https://cdn.is-a.fun/mewwme/mewwme.png")
-        .setProgress(10)
-        .setStartTime("0:00")
-        .setEndTime("04:27")
-
-    const cardBuffer = await card.build();
-
-    fs.writeFileSync(`mewcard.png`, cardBuffer);
-    console.log("Done!");
-})()
+    
+        const card = new mewcard()
+            .setName("Bunga Hati")
+            .setAuthor("Salma Salsabil")
+            .setColor("auto")
+            .setTheme("classic")
+            .setBrightness(50)
+            .setThumbnail("https://cdn.discordapp.com/attachments/1194410866209206412/1196152611594838026/mewwme.png")
+            .setRequester("romanromannya")
+    
+        const cardBuffer = await card.build();
+    
+        fs.writeFileSync(`mewcard.png`, cardBuffer);
+        console.log("Done!");
+    })()
