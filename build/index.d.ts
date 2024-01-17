280f25afa86da8f30a25db2c65bd3fc6a518f35a
lrmn7 <lrmn.dev@gmail.com>
2024-01-17 21:46:23 +0700

    🚑

--- build/index.d.ts
+++ build/index.d.ts
@@ -3,7 +3,7 @@ export declare class mewcard {
     name?: string;
     author?: string;
     color?: string;
-    theme?: "mewwme" | "themes1" | "themes2" | "themes3" | "themes4" | "themes5" | "themes6" | "themes7" | "themes8" | "themes9" | "themes10" | "themes11" | "themes12" | "themes13" | "themes14";
+    theme?: "mewwme" | "themes1" | "themes2" | "themes3" | "themes4" | "themes5" | "themes6" | "themes7" | "themes8" | "themes9" | "themes10" | "themes11" | "themes12" | "themes13" | "themes14" | "themes15" | "themes16";
     brightness?: number;
     thumbnail?: string;
     requester?: string;
@@ -12,7 +12,7 @@ export declare class mewcard {
   public setName(name: string): this;
   public setAuthor(author: string): this;
   public setColor(color: string): this;
-  public setTheme(theme: string | "mewwme" | "themes1" | "themes2" | "themes3" | "themes4" | "themes5" | "themes6" | "themes7" | "themes8" | "themes9" | "themes10" | "themes11" | "themes12" | "themes13" | "themes14"): this;
+  public setTheme(theme: string | "mewwme" | "themes1" | "themes2" | "themes3" | "themes4" | "themes5" | "themes6" | "themes7" | "themes8" | "themes9" | "themes10" | "themes11" | "themes12" | "themes13" | "themes14" | "themes15" | "themes16"): this;
   public setBrightness(brightness: number): this;
   public setThumbnail(thumbnail: string): this;
   public setRequester(requester: string): this;
