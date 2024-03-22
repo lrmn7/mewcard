2aab79983f5950c0415e74ccc6b2d4a79971c420
lrmn7 <lrmn.dev@gmail.com>
2024-03-23 00:03:35 +0700

    Update mewcard structure: add themes19 support

--- build/structures/mewcard.js
+++ build/structures/mewcard.js
@@ -2149,7 +2149,7 @@ class mewcard {
 
             // Fungsi untuk menghasilkan warna heksadesimal acak
             // Array warna yang diizinkan
-            const allowedColors = ['#f2d7b7', '#fbc5f9', '#00ff2a', '#ff00a8', '#00ffe4', '#ff6000'];
+            const allowedColors = ['#f2d7b7'];
 
             // Fungsi untuk memilih warna secara acak dari array di atas
             function getRandomColor() {
@@ -2178,8 +2178,102 @@ class mewcard {
 
             return frame.toBuffer("image/png");
 
+        } else if (this.theme === "themes19") {
+                const frame = canvas.createCanvas(800, 200);
+                const ctx = frame.getContext("2d");
+    
+                const circleCanvas = canvas.createCanvas(1000, 1000);
+                const circleCtx = circleCanvas.getContext('2d');
+    
+                const circleRadius = 20;
+                const circleY = 97;
+    
+                // Daftar URL gambar yang dapat dipilih secara acak
+                const imageUrls = [
+                    "https://cdn.is-a.fun/mewcard/themes19/1.png",
+                ];
+    
+                // Fungsi untuk memilih URL gambar secara acak
+                function getRandomImageUrl() {
+                    const randomIndex = Math.floor(Math.random() * imageUrls.length);
+                    return imageUrls[randomIndex];
+                }
+    
+                // Mengambil gambar secara acak
+                const backgroundUrl = getRandomImageUrl();
+                const background = await canvas.loadImage(backgroundUrl);
+                ctx.drawImage(background, 0, 0, frame.width, frame.height);
+    
+                const thumbnailCanvas = canvas.createCanvas(800, 200); // Mengubah lebar kanvas
+                const thumbnailCtx = thumbnailCanvas.getContext('2d');
+    
+                let thumbnailImage;
+    
+                try {
+                    thumbnailImage = await canvas.loadImage(this.thumbnail, {
+                        requestOptions: {
+                            headers: {
+                                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+                            }
+                        }
+                    });
+                } catch (error) {
+                    // Mengatasi kesalahan ketika gambar tidak dapat dimuat
+                    console.error('MUSICARD: Thumbnail image failed to load, not supported [Lofi & Radio]');
+                    thumbnailImage = await canvas.loadImage(`https://cdn.is-a.fun/mewcard/themes19/thumbnail.png`); // Gunakan gambar default atau URL alternatif
+                }
+    
+                const thumbnailSize = Math.min(thumbnailImage.width, thumbnailImage.height);
+                const thumbnailX = (thumbnailImage.width - thumbnailSize) / 2;
+                const thumbnailY = (thumbnailImage.height - thumbnailSize) / 2;
+    
+    
+    
+                thumbnailCtx.drawImage(thumbnailImage, thumbnailX, thumbnailY, thumbnailSize, thumbnailSize, 0, 0, thumbnailCanvas.width, thumbnailCanvas.height);
+    
+    
+                // Menggambar thumbnail
+                ctx.drawImage(thumbnailCanvas, 50, 40, 180, 130);
+    
+                // Menambahkan border putih
+                ctx.strokeStyle = '#f2d7b7'; // Warna border putih
+                ctx.lineWidth = 5; // Lebar border (sesuaikan dengan preferensi Anda)
+                ctx.strokeRect(50, 40, 180, 130); // Koordinat dan ukuran border
+    
+    
+                // Fungsi untuk menghasilkan warna heksadesimal acak
+                // Array warna yang diizinkan
+                const allowedColors = ['#f2d7b7', '#fbc5f9', '#00ff2a', '#ff00a8', '#00ffe4', '#ff6000'];
+    
+                // Fungsi untuk memilih warna secara acak dari array di atas
+                function getRandomColor() {
+                    return allowedColors[Math.floor(Math.random() * allowedColors.length)];
+                }
+    
+                // Mengatur warna teks secara acak dari array yang diizinkan
+                ctx.font = "bold 38px circular-std, noto-emoji, noto-sans-jp, noto-sans, noto-sans-kr";
+                ctx.fillStyle = getRandomColor(); // Menggunakan fungsi untuk warna acak dari array yang diizinkan
+                ctx.fillText(this.name, 250, 100);
+    
+                // Teks "author" dengan warna dan ukuran font yang berbeda
+                const authorText = this.author;
+                ctx.fillStyle = getRandomColor(); // Menggunakan fungsi untuk warna acak dari array yang diizinkan
+                ctx.font = "bold 22px circular-std, noto-emoji, noto-sans-jp, noto-sans, noto-sans-kr";
+                ctx.fillText(authorText, 250, 140);
+    
+                // Mengukur lebar teks "author" untuk menentukan posisi teks "requester"
+                const authorTextWidth = ctx.measureText(authorText).width;
+    
+                // Teks "requester" dengan warna dan ukuran font yang berbeda
+                const requesterText = this.requester;
+                ctx.fillStyle = getRandomColor(); // Menggunakan fungsi untuk warna acak dari array yang diizinkan
+                ctx.font = "bold 22px circular-std, noto-emoji, noto-sans-jp, noto-sans, noto-sans-kr";
+                ctx.fillText(requesterText, 250 + authorTextWidth + 10, 140); // Mengatur posisi "requester" setelah "author"
+    
+                return frame.toBuffer("image/png");
+
         } else {
-            throw new Error('Invalid theme parameter, must be "mewwme" | "themes1" | "themes2" | "themes3" | "themes4" | "themes5" | "themes6" | "themes7" | "themes8" | "themes9" | "themes10" | "themes11" | "themes12" | "themes13" | "themes14" | "themes15" | "themes16"');
+            throw new Error('Invalid theme parameter, must be "mewwme" | "themes1" | "themes2" | "themes3" | "themes4" | "themes5" | "themes6" | "themes7" | "themes8" | "themes9" | "themes10" | "themes11" | "themes12" | "themes13" | "themes14" | "themes15" | "themes16" | "themes17" | "themes18" | "mewwme1" | "themes19"');
         }
     }
 }
