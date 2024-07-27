63a8cca3dee3291ca4cb9f117a31a93bb95a5e62
lrmn7 <lrmn.dev@gmail.com>
2024-07-27 19:35:32 +0700

    Added 'bebe' theme to mewcard, updated theme options in index.d.ts and mewcard.js, and bumped version to 1.7.5 in package.json

--- build/index.d.ts
+++ build/index.d.ts
@@ -3,7 +3,7 @@ export declare class mewcard {
     name?: string;
     author?: string;
     color?: string;
-    theme?: "mewwme" | "mewwme1" | "themes1" | "themes2" | "themes3" | "themes4" | "themes5" | "themes6" | "themes7" | "themes8" | "themes9" | "themes10" | "themes11" | "themes12" | "themes13" | "themes14" | "themes15" | "themes16" | "themes17" | "themes18" | "themes19" | "miko" | "blank";
+    theme?: "mewwme" | "bebe" | "mewwme1" | "themes1" | "themes2" | "themes3" | "themes4" | "themes5" | "themes6" | "themes7" | "themes8" | "themes9" | "themes10" | "themes11" | "themes12" | "themes13" | "themes14" | "themes15" | "themes16" | "themes17" | "themes18" | "themes19" | "miko" | "blank";
     brightness?: number;
     thumbnail?: string;
     requester?: string;
@@ -12,7 +12,7 @@ export declare class mewcard {
   public setName(name: string): this;
   public setAuthor(author: string): this;
   public setColor(color: string): this;
-  public setTheme(theme: string | "mewwme" | "mewwme1" | "themes1" | "themes2" | "themes3" | "themes4" | "themes5" | "themes6" | "themes7" | "themes8" | "themes9" | "themes10" | "themes11" | "themes12" | "themes13" | "themes14" | "themes15" | "themes16" | "themes17" | "themes18" | "themes19" | "miko" | "blank"): this;
+  public setTheme(theme: string | "mewwme" | "bebe" | "mewwme1" | "themes1" | "themes2" | "themes3" | "themes4" | "themes5" | "themes6" | "themes7" | "themes8" | "themes9" | "themes10" | "themes11" | "themes12" | "themes13" | "themes14" | "themes15" | "themes16" | "themes17" | "themes18" | "themes19" | "miko" | "blank"): this;
   public setBrightness(brightness: number): this;
   public setThumbnail(thumbnail: string): this;
   public setRequester(requester: string): this;
